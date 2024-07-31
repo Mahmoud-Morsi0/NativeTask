af06934a694969d7cf5760b91dcde3a06b8d6161
Morsi <136155900+Mahmoud-Morsi0@users.noreply.github.com>
2024-07-31 18:27:56 +0300

    complete table

---
+++ js/Table.js
@@ -0,0 +1,17 @@
+export default class Table {
+    constructor(user) {
+        this.dateOfBirth = user.dateOfBirth;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+    }
+
+    render() {
+        return `
+      <tr>
+        <td>${this.dateOfBirth}</td>
+        <td>${this.firstName}</td>
+        <td>${this.lastName}</td>
+      </tr>
+    `;
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,75 @@
+import Table from "./Table.js";
+
+
+
+
+const getData = async () => {
+    try {
+        const res = await fetch('../data.json');
+        const data = await res.json();
+        createTable(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+getData();
+const groupDataByYear = (data) => {
+    const groupedData = {};
+    const uniqueYearsData = [];
+
+    data.forEach(entry => {
+        const year = entry.dateOfBirth.split('-')[0];
+        if (groupedData[year]) {
+            groupedData[year].push(entry);
+        } else {
+            groupedData[year] = [entry];
+        }
+    });
+
+    for (const year in groupedData) {
+        if (groupedData[year].length === 1) {
+            uniqueYearsData.push(groupedData[year][0]);
+            delete groupedData[year];
+        }
+    }
+
+    if (uniqueYearsData.length > 0) {
+        groupedData['unique years'] = uniqueYearsData;
+    }
+
+    return groupedData;
+};
+
+const createTable = (data) => {
+    const groupedData = groupDataByYear(data);
+    let tableHTML = `<table  
+    class="table table-striped table-hover table-borderless  align-middle">
+    <thead>
+        <tr class="table-primary">
+           <th>Date of Birth</th>
+           <th>First Name</th>
+           <th>Last Name</th>
+        </tr>
+    </thead>
+    <tbody>`;
+
+    for (const year in groupedData) {
+        const entries = groupedData[year];
+
+        tableHTML += `<tr class="table-primary">
+                        <td colspan="3" style="font-weight: bold;">${year}</td>
+                      </tr>
+                      `;
+
+        entries.forEach(entry => {
+            const row = new Table(entry);
+            tableHTML += row.render();
+        });
+    }
+
+    tableHTML += '</tbody></table>';
+    document.body.innerHTML = tableHTML;
+};
+
+
